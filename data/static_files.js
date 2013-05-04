37871174f2ab8bf70c7c532f2b914b4bbd9d9152
Alexander Ruliov <rulix.exec@gmail.com>
2013-05-04 22:47:02 +0300

    tags and communa project updates
    Conflicts:
    	static

--- data/static_files.js
+++ data/static_files.js
@@ -1,3 +1,6 @@
 module.exports = [
-    'images/tags/2013-04-30-root.png', 'images/tags/2013-05-01-join.png'
-];
+    '2013-04-30-root.png', '2013-05-01-join.png',
+    '2013-05-04-me.png', '2013-05-04-find.png'
+].map(function(tag) {
+    return 'images/tags/' + tag;
+});
